7ef48725a2360405d9b6aec59492aaf7004d477c
Dawid Bulinski (PGS Software) <dbulinski@pgs-soft.com>
2021-05-05 10:32:38 +0200

    Completed task 4

--- myApp.js
+++ myApp.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+app.use("/public", express.static("public"));
+
 app.get("/", (_, res) => {
  res.sendFile(path.join(__dirname, "views", "index.html"));
 })
