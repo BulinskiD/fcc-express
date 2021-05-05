f2ac6f3761197be18d9e3432629df2c1141e4c94
Dawid Bulinski (PGS Software) <dbulinski@pgs-soft.com>
2021-05-05 10:44:46 +0200

    Completed task 6 (dotenv)

--- myApp.js
+++ myApp.js
@@ -1,52 +1,23 @@
-const express = require('express');
-const path = require('path');
+require("dotenv").config();
+
+const express = require("express");
+const path = require("path");
 const app = express();
 
 app.use("/public", express.static("public"));
 
 app.get("/", (_, res) => {
- return res.sendFile(path.join(__dirname, "views", "index.html"));
-})
+  return res.sendFile(path.join(__dirname, "views", "index.html"));
+});
 
 app.get("/json", (_, res) => {
- return res.json( { message: "Hello json"});
-})
+  let message = "Hello json";
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    message = message.toLocaleUpperCase();
+  }
+  return res.json({ message });
+});
 
 console.log("Hello world");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
